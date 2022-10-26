c07da8abca8b7f0641b6cc134b78382c0023350a
MoMo R <moran.risk78@gmail.com>
2022-10-25 23:36:28 -0700

    input valication added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # team-profile-generator
+
+## credit
+- email validation
+https://www.w3resource.com/javascript/form/email-validation.php
--- index.js
+++ index.js
@@ -22,90 +22,109 @@ const generateHtml = require('./src/generateHtml')
 // myTeamMemberArr.push(z)
 // myTeamMemberArr.push(v)
 // myTeamMemberArr.push(c)
+
+// this array will all types of member class objects , manager, engineer, intern.
 const myTeamMemberArr = [];
 
 
 
-
+// prompt managers information and start building team
+// create new Manager and push it to myTeamMemberArr
+// manager's name is required. valid email check
 function init() {
+    console.log(`❇ Welcome to the Team Profile Generator ❇\n `)
+    console.log(`Please enter the team manager's information `)
     inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: `Team Manager's Name`
+            message: `The manager's name:`,
+            validate(input) { return answerRequired(input); } 
         },
         {
             type: 'input',
             name: 'id',
-            message: `Employee ID`
+            message: `The manager's employee ID:`
         },
         {
             type: 'input',
             name: 'email',
-            message: `Email Address:`
+            message: `The manager's email address:`,
+            validate(input) { return emailValidate(input) }
         },
         {
             type: 'input',
             name: 'office',
-            message: `Office Number`
+            message: `The manager's office number:`
         }
     ]).then( (ans) => {
         const newMng = new Manager(ans.name, ans.id, ans.email, ans.office);
         myTeamMemberArr.push(newMng);
        
-        askQuestion() 
+        askQuestion();
     })
 
 }
 
+// ask if you want to add engineer/ intern / finish
+// call functions accordingly to get more info of the team member / finish and create html
 function askQuestion()  {
+    console.log(``)
     inquirer.prompt([
         {
             type: 'list',
             name: 'addTeam',
-            message: `What do you want to do next?`,
+            message: `What type of member would you like to add?`,
             choices: [
-                'Add an engineer',
-                'Add an intern',
+                'Engineer',
+                'Intern',
                 'Finish building my team'
             ]
         }
     ]).then(({addTeam}) => {
         switch(addTeam) {
-            case 'Add an engineer' :
+            case 'Engineer' :
                 addEngineer();
                 break;
-            case 'Add an intern' :
+            case 'Intern' :
                 addIntern();
                 break;
             case 'Finish building my team' :
                 createTeamHTML();
                 break;
+            default:
+
         }
     })
 }
 
+// prompt engineer's information and create new Engineer and push it to myTeamMemberArr
+// name, email, github valid check
 function addEngineer() {
+    console.log(`Please enter the engineer's information `)
     inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: `Engineer's name?`,
+            message: `Engineer's name:`,
+            validate(input) { return answerRequired(input); } 
         },
         {
             type: 'input',
             name: 'id',
-            message: `Employee ID`
+            message: `Engineer's employee ID:`
         },
         {
             type: 'input',
             name: 'email',
-            message: `Email Address:`
+            message: `Engineer's email address:`,
+            validate(input) { return emailValidate(input) }
         },
         {
             type: 'input',
             name: 'github',
-            message: `Github username:`
+            message: `Github username:`,
+            validate(input) { return answerRequired(input); } 
         }
 
     ]).then(ans => {
@@ -115,42 +134,57 @@ function addEngineer() {
     })
 }
 
+// prompt intern's information and create new Intern and push it to myTeamMemberArr
+// name, email, github valid check
 function addIntern(){
+    console.log(`Please enter the intern's information `)
     inquirer.prompt([
         {
             type: 'input',
             name: 'name',
-            message: `Intern's name?`,
+            message: `Intern's name:`,
+            validate(input) { return answerRequired(input); } 
         },
         {
             type: 'input',
             name: 'id',
-            message: `Employee ID`
+            message: `Intern's employee ID:`
         },
         {
             type: 'input',
             name: 'email',
-            message: `Email Address:`
+            message: `Intern's email address:`,
+            validate(input) { return emailValidate(input) }
         },
         {
             type: 'input',
             name: 'school',
-            message: `School:`
+            message: `Intern's school:`
         }
     ]).then(ans => {
         const newIntern= new Intern(ans.name, ans.id, ans.email, ans.school);
         myTeamMemberArr.push(newIntern);
-        askQuestion() 
+        askQuestion();
     })
 
 }
 
-function createTeamHTML(){
-
-    console.log(myTeamMemberArr)
-
-    // console.log(generateHtml(myTeamMemberArr));
-
-    fs.writeFile('./dist/team.html', generateHtml(myTeamMemberArr), (err)=> err? console.error(err): console.log('Team HTML created!'))
+function emailValidate(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)){
+        return true;
+      }
+    return `You have entered an invalid email address!`;
 }
+function answerRequired(input) {
+    if (!input) {
+        return `This field is required!`;
+    } return true;
+}
+
+
+function createTeamHTML(){
+    // console.log(myTeamMemberArr)
+    fs.writeFile('./dist/team.html', generateHtml(myTeamMemberArr), (err)=> err? console.error(err): console.log('❇ Team Profile HTML is created! Check dist/team.html❇'))
+}
+
 init ();
